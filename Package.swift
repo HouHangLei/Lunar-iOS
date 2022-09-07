69187608af055105aedb249c94eec8352721d9c2
hl <1715185444@qq.com>
2022-09-07 20:28:24 +0800

    支持SwiftPM

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Lunar-iOS",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Lunar-iOS",
+            targets: ["Lunar-iOS"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Lunar-iOS",
+            dependencies: [],
+            path: "Lunar-iOS",
+            resources: [.process("Lunar-iOS.bundle")]
+        ),
+    ]
+)
